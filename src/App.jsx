ece062fdb8fd5c086136eea1e3456c5eae9877e5
mohamed <ouzammad.mohamed@gmail.com>
2024-12-02 16:41:45 +0100

    add_new_img-and-css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import PaintingDisplay from './PaintingDisplay';
+import './App.css';
 
 function App() {
   const paintings = [
@@ -17,10 +18,20 @@ function App() {
       author: "Dirck Hals",
       imageUrl: "https://lh3.googleusercontent.com/zjehg1KWc2C2s4c3ThD3v1ROk8PFjj2c-6ZcIoLzmBp-bQ17AkF7EijTqWuXx6YbGWrAJA04Tqm3AVcLsqEifHEyMAbEoZ9ROiUUZjbYNw=s0",
     },
+    {
+      title: "River Landscape with Riders",
+      author: "Aelbert Cuyp",
+      imageUrl: "https://lh3.googleusercontent.com/X7_CHCjksOZYu4gIGa45Edj1tMymdiz2o3pbL6HqqVEszWvPzrM6iIwHzaWNqgsWLcm7VmHCQyuQowWSSImQYLF8qW48zmZ-rx309F3c=s0",
+    },
+    {
+      title: "The Massacre of the Innocents",
+      author: "Cornelis Cornelisz. van Haarlem",
+      imageUrl: "https://lh5.ggpht.com/JH0svNh0Pkov_W97MDHw8v2-qKS8AdixVJ-CiPL_xBECNdEyTBkicMvZBsqgW6GQ0TB9moKnfGUYacWQS32rqeoEjA4=s0",
+    },
   ];
 
   return (
-    <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center", gap: "20px", padding: "20px" }}>
+    <div className="painting-grid">
       {paintings.map((painting, index) => (
         <PaintingDisplay
           key={index}
