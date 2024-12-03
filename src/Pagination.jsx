bd26246fb0923ed7e4c6ae64409a65355974a33e
mohamed <ouzammad.mohamed@gmail.com>
2024-12-03 16:11:49 +0100

    add_color

---
+++ src/Pagination.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Pagination({ currentPage, totalPages, onNext, onPrevious, onReset }) {
+    return (
+        <div className="pagination">
+            <button onClick={onPrevious} disabled={currentPage === 1}>
+                Précédent
+            </button>
+            <span>
+                Page {currentPage} sur {totalPages}
+            </span>
+            <button onClick={onNext} disabled={currentPage === totalPages}>
+                Suivant
+            </button>
+            <button onClick={onReset}>Réinitialiser</button>
+        </div>
+    );
+}
+
+export default Pagination;
